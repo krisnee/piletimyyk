9f70051249b252b9d48657d04cbbc5a48e9a0b1f
Kris Nee <kristi.neemesto@gmail.com>
2025-01-14 22:06:23 +0200

    Delivery https://trello.com/c/bRtUPwkV/8-as-an-developer-i-want-to-get-a-list-of-all-events-to-show-in-my-app

--- piletimyyk-api/index.js
+++ piletimyyk-api/index.js
@@ -5,13 +5,25 @@ const swaggerUi = require('swagger-ui-express');
 const yamljs = require('yamljs');
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 
+const events = [
+    "Laulu- ja tantsupidu XX",
+    "Tallinna Maraton",
+    "Simple Session",
+    "Tallinna Vanalinna Päevad",
+    "Jõulujazz",
+    "Tallinn Music Week",
+    "Tallinna Merepäevad"
+]
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.get('/tickets', (req, res) => {
-    res.send(["Laulu- ja tantsupidu XX","45€","2025-07-11"]);
+app.get('/events', (req, res) => {
+    res.send([events]);
+});
+app.get('/events/:id', (req, res) => {
+    res.send(games[req.params.id]);
 });
 
 app.listen(port, () => {
     console.log(`Server töötab aadressil http://localhost:${port}`);
 });
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));

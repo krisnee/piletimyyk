3fc8c02c3626b7d68b6185308435b1364234e57b
LAPTOP-8BL927LI\kartt <karttriin@gmail.com>
2025-02-17 15:08:44 +0200

    DELIVERS https://trello.com/c/ef7ZR5ov/4-as-an-user-i-want-to-get-full-details-of-a-given-single-ticket-to-show-in-my-app

--- piletimyyk-api/controllers/UsersController.js
+++ piletimyyk-api/controllers/UsersController.js
@@ -5,3 +5,10 @@ exports.getAll = async (req, res) => {
     const users = await db.User.findAll();
     res.status(200).send(users.map(({ user_id, first_name, last_name, email }) => ({ user_id, first_name, last_name, email })));
 }
+exports.getById = async (req, res) => {
+    const user = await db.User.findByPk(req.params.id);
+    if (!user) {
+        return res.status(404).send({ error: "User not found" });
+    }
+    res.status(200).send(user);
+}

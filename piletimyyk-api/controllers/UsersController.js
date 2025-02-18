85cfee7a260400738d140b74cf2b686c96204f86
LAPTOP-8BL927LI\kartt <karttriin@gmail.com>
2025-02-18 11:21:32 +0200

    DELIVERS https://trello.com/c/dGmEtEO1/31-as-a-developer-i-am-implementing-user-authentication-login-logout-register

--- piletimyyk-api/controllers/UsersController.js
+++ piletimyyk-api/controllers/UsersController.js
@@ -46,3 +46,11 @@ exports.deleteById = async (req, res) => {
     await user.destroy();
     res.status(204).send();
 }
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await db.User.findOne({ where: { email, password } });
+    if (!user) {
+        return res.status(401).send({ message: "Invalid email or password" });
+    }
+    res.status(200).send({ message: "Login successful" });
+};
